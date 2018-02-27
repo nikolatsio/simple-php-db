dd1a90a172571068fbb7e80b0d7ecaf7cad6b28f
Nikolay Nikolov <niki@tintirimintiri.org>
2018-02-27 13:58:45 +0200

    update dbtest

--- dbtest.php
+++ dbtest.php
@@ -1,9 +1,9 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("MYSQL_USER");
-$dbpwd = getenv("MYSQL_PASSWORD");
-$dbname = getenv("MYSQL_DATABASE");
+$dbuser = getenv("testuser");
+$dbpwd = getenv("testpwd");
+$dbname = getenv("testdb");
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
