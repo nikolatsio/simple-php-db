c6be9cfd5134ddcc885857ce7effe2b1555afcda
Nikolay Nikolov <niki@tintirimintiri.org>
2018-02-27 14:23:31 +0200

    update dbtest -2

--- dbtest.php
+++ dbtest.php
@@ -10,15 +10,16 @@ if ($connection->connect_errno) {
     printf("Connection failed: %s\n", $mysqli->connect_error);
     exit();
 }
-echo "Just for fun!";
+echo "Just for fun!<br /><br />";
 
 $query = "SELECT * from users";
 
 $rs = $connection->query($query);
 	while ($row = $rs->fetch_array(MYSQLI_ASSOC)) {
-    		echo $row['user_id'] . " " . $row['username'] . "\n";
+    		echo $row['user_id'] . " " . $row['username'] . "<br/>";
 	}
 
+echo "<br/>Its just a test for connection between php and mysql in OpenStack<br/>Nikolay Nikolov @ 2018";
 $rs->close();
 $connection->close();
 ?>
